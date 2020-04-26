bd4a5f1be21f86026d3f11ec27ccdc89c28aaa38
t0nxx <mahmoudko1500@hotmail.com>
2020-04-26 23:52:07 +0200

    remove signup image from ios

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useContext } from 'react';
-import { StyleSheet, Text, AsyncStorage, TextInput, TouchableHighlight, View, Image, Linking } from 'react-native';
+import { StyleSheet, Text, AsyncStorage, TextInput, TouchableHighlight, View, Image, Linking, Platform } from 'react-native';
 import { http, checkCurrentUser } from '../services/httpService'
 import { Container, Toast, Button, Thumbnail, CheckBox } from 'native-base';
 import * as ImagePicker from 'expo-image-picker';
@@ -41,26 +41,33 @@ export const SignUpScreen = ({ navigation }) => {
     const [isLogin, setIsLogin] = useContext(AuthContext);
 
     const selectPicture = async () => {
-        try {
-            await Permissions.askAsync(Permissions.CAMERA_ROLL);
-            const pick = await ImagePicker.launchImageLibraryAsync({
-                mediaTypes: ImagePicker.MediaTypeOptions.Images,
-                aspect: [3, 3],
-                allowsEditing: true,
-                base64: true
-            });
-            if (pick.cancelled == false) {
-                let filename = pick.uri.split('/').pop();
-                let fileExtention = filename.split('.')[1];
-                console.log(fileExtention)
-                const file = `data:image/${fileExtention};base64,${pick.base64}`;
-                setImg(file);
+        if (Platform.OS == 'android') {
+            try {
+                const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+                if (status !== 'granted') {
+                    alert('Hey! Hakaya need camera and photos library permission to set your profile picture.');
+                }
+                const pick = await ImagePicker.launchImageLibraryAsync({
+                    mediaTypes: ImagePicker.MediaTypeOptions.Images,
+                    aspect: [3, 3],
+                    allowsEditing: true,
+                    base64: true,
+
+                });
+                if (pick.cancelled == false) {
+                    let filename = pick.uri.split('/').pop();
+                    let fileExtention = filename.split('.')[1];
+                    console.log(fileExtention)
+                    const file = `data:image/${fileExtention};base64,${pick.base64}`;
+                    setImg(file);
+                }
+
+
+            } catch (error) {
+                console.log(error);
             }
-
-
-        } catch (error) {
-            console.log(error);
         }
+
         // if (!cancelled) setImg(uri);
     };
 
@@ -76,7 +83,7 @@ export const SignUpScreen = ({ navigation }) => {
                 if (img.length > 3) {
                     body.img = img;
                 }
-                if(isAccept !== true) {
+                if (isAccept !== true) {
                     throw new Error('يرجي الموافقه على الشروط والاحكام')
                 }
                 const { data } = await http.post('/users/new', body);
@@ -113,14 +120,17 @@ export const SignUpScreen = ({ navigation }) => {
         <View style={styles.container}>
             {/* <Image style={{ bottom: 30 }} source={require('../assets/images/logo.png')} /> */}
 
-            <TouchableHighlight onPress={() => selectPicture()}>
-                <>
-                    {img.length > 3 ? <Thumbnail style={{ width: 120, height: 120, borderRadius: 120 / 2 }} source={{ uri: img }} />
-                        : <Thumbnail style={{ width: 120, height: 120, borderRadius: 120 / 2, backgroundColor: 'white' }} source={require('../assets/images/test/user-big.png')} />
-                    }
-                    <Text style={{ color: 'white', fontFamily: 'Cairo', paddingBottom: 5 }}> اختر صورة </Text>
-                </>
-            </TouchableHighlight>
+            {Platform.OS == 'android' ?
+                <TouchableHighlight onPress={() => selectPicture()}>
+                    <>
+                        {img.length > 3 ? <Thumbnail style={{ width: 120, height: 120, borderRadius: 120 / 2 }} source={{ uri: img }} />
+                            : <Thumbnail style={{ width: 120, height: 120, borderRadius: 120 / 2, backgroundColor: 'white' }} source={require('../assets/images/test/user-big.png')} />
+                        }
+                        <Text style={{ color: 'white', fontFamily: 'Cairo', paddingBottom: 5 }}> اختر صورة </Text>
+                    </>
+                </TouchableHighlight>
+                : null}
+
             <View style={styles.inputContainer}>
                 <TextInput style={[styles.inputs, { textAlign: 'center' }]}
                     placeholder=" اسم المستخدم    "
@@ -159,14 +169,14 @@ export const SignUpScreen = ({ navigation }) => {
                     textDecorationLine: 'underline'
                 }}
                 onPress={() => { Linking.openURL(`https://hakaya.news/terms.html`) }}
-                // >{data.source.name}</Text>
-                > الشروط والاحكام </Text>
-                <Text style={{ fontFamily: 'Cairo', color: 'white' ,margin :5 , textTransform : 'uppercase' }}> i have read the terms and completly</Text>
-                <View style={{ flexDirection: 'row', marginBottom: 3  }}>
-                    
-                    <Text style={{ fontFamily: 'Cairo', color: 'white' ,textTransform : 'uppercase'}}>  agree on the above terms and eula </Text>
-                    <CheckBox checked={isAccept} color="white" onPress={() => setIsAccept(!isAccept)} />
-                </View>
+            // >{data.source.name}</Text>
+            >  Terms and Condations </Text>
+            <Text style={{ fontFamily: 'Cairo', color: 'white', margin: 5, textTransform: 'uppercase' }}> i have read the terms and completly</Text>
+            <View style={{ flexDirection: 'row', marginBottom: 3 }}>
+
+                <Text style={{ fontFamily: 'Cairo', color: 'white', textTransform: 'uppercase' }}>  agree on the above terms and eula </Text>
+                <CheckBox checked={isAccept} color="white" onPress={() => setIsAccept(!isAccept)} />
+            </View>
 
 
             <TouchableHighlight style={[styles.buttonContainer, { backgroundColor: '#742A99' }]} onPress={() => SignUp()}>
